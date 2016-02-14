4769e3fac141fd7feac97ea07989c5b3cb9c1dac
katiecampbell <katiec@trilliumpublishing.com>
2016-02-14 15:24:59 -0800

    Final project update

---
+++ Trillium Website/js/Trillium_home.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	console.log("js loaded");
+		$('.samples_carousel').slick({
+	});
+});
+
+$('.autoplay').slick({
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+});
