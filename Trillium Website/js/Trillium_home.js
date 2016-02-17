6311e6ebc73b2c6b2f1ef1f92664aee1425b06db
katiecampbell <katiec@trilliumpublishing.com>
2016-02-17 15:36:21 -0800

    trillium update

--- Trillium Website/js/Trillium_home.js
+++ Trillium Website/js/Trillium_home.js
@@ -4,9 +4,15 @@ $(document).ready(function(){
 	});
 });
 
-$('.autoplay').slick({
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 2000,
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 6
 });
+
+// $('.autoplay').slick({
+//           slidesToShow: 3,
+//           slidesToScroll: 1,
+//           autoplay: true,
+//           autoplaySpeed: 2000,
+// });
