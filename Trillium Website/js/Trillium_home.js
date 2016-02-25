817540ae8360b1379157ffc64a04c1f622036a7a
katiecampbell <katiec@trilliumpublishing.com>
2016-02-24 18:50:54 -0800

    Final project update

--- Trillium Website/js/Trillium_home.js
+++ Trillium Website/js/Trillium_home.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	console.log("js loaded");
 	$('.myAwesomeCarousel').slick({
 		autoplay: false,
-		slidesToShow: 3,
+		slidesToShow: 2,
 		
 	});
 });
@@ -19,3 +19,14 @@ $('.autoplay').slick({
           autoplay: true,
           autoplaySpeed: 2000,
 });
+
+$(".icons").mouseover(function(){
+   	$(this).find("p").stop().slideDown("slow");
+});
+$(".icons").mouseout(function(){
+	$(this).find("p").stop().slideUp("slow");
+});
+
+// jQuery find method 
+// use this keyword
+// https://api.jquery.com/find/
